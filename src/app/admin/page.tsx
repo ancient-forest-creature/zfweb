8e702e4aa4637886ab806d5162bf29dbf737daaa
Lagomorph <godzilka@gmail.com>
2024-12-31 11:12:58 -0800

    Trying to fix build issue by adding Admin page content

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main >
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-4xl font-bold text-center">
+          Admin Page
+        </h1>
+
+      </div>
+    </main>
+  );
+}

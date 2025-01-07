a5943112be0f42941a66076c232d677ba052fb4d
Lagomorph <godzilka@gmail.com>
2025-01-06 16:07:53 -0800

    fix deploy issue

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,12 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main >
+    <main>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-4xl font-bold text-center">
-          Admin Page
-        </h1>
-
+        <h1 className="text-center text-4xl font-bold">Admin Page</h1>
       </div>
     </main>
   );

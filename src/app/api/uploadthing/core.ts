c2ffd39ccdfaa7b402b9cbebb59c6936226b02cc
Lagomorph <godzilka@gmail.com>
2025-01-06 17:43:40 -0800

    added the beginnings of file upload

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/only-throw-error */
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { db } from "~/server/db";
+import { product } from "~/server/db/schema";
+//import { ratelimit } from "~/server/ratelimit";
+
+const f = createUploadthing();
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+    image: {
+      /**
+       * For full list of options and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "8MB",
+      maxFileCount: 4,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await auth();
+
+      // If you throw, the user will not be able to upload
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await (
+        await clerkClient()
+      ).users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError("User Does Not Have Upload Permissions");
+
+      //   const { success } = await ratelimit.limit(user.userId);
+      //   if (!success) {
+      //     throw new UploadThingError("Rate limit exceeded");
+      //   }
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      //   await db.insert(product).values({
+      //     utkey: file.key,
+      //     url: file.url,
+      //     name: file.name,
+      //     userId: metadata.userId,
+      //   });
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
